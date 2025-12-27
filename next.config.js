5d7962da62c70ed2490f7b22a8bc919f5c48341f
Oussama Chaoui <oussamaqqqq@gmail.com>
2025-12-27 16:43:48 +0100

    chore: update Next.js configuration for admin interface
    - Set basePath to '/admin' for routing adjustments.
    - Enabled trailingSlash for consistent URL formatting.
    - Removed outdated remotePatterns from images configuration.

--- next.config.js
+++ next.config.js
@@ -2,15 +2,11 @@
 const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
-  // output: 'export',
   reactStrictMode: false,
+  basePath: '/admin',
+  trailingSlash: true,
   images: {
     domains: ['localhost', 'flagcdn.com'],
-    remotePatterns: [
-      {
-        hostname: '**.cyber-scale.me',
-      },
-    ],
   },
   i18n,
 };
